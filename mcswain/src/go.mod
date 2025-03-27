77df00fb04b1bda9affffc2e98a999374a9be188
John McSwain <johnmcswain@users.noreply.github.com>
2025-03-27 17:48:34 -0400

    Delete mcswain/src directory

--- mcswain/src/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/jmcswain/cli/mcswain
-
-go 1.21
-
-require (
-    github.com/spf13/cobra v1.6.1
-)
--- mcswain/src/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/johnmcswain/cli/mcswain/src/cmd"
-
-func main() {
-    cmd.Execute()
-}
